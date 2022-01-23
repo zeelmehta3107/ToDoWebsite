23507993bd24d9cc08ca873c160150e5570e7ccb
zeelmehta3107 <75499166+zeelmehta3107@users.noreply.github.com>
2022-01-23 21:56:20 +0530

    Update urls.py

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ToDo.urls')),
+    path('/', include('ToDo.urls')),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
