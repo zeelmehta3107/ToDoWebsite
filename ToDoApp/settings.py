43c6c5406975b89b32fc7db42acced2687e080b8
zeelmehta3107 <75499166+zeelmehta3107@users.noreply.github.com>
2022-07-19 20:29:31 +0530

    Update settings.py

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@5mp$5n@4(ykvsvktueh-cxae4yoiorkcpz_eiu9g$a-ylemrp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','todoappbyzeel.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','todoappbyzeel.herokuapp.com','.todoappbyzeel.com']
 
 
 # Application definition
